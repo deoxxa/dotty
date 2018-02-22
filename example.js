8e157c2cd0508874c29cc80166ff4bd8ac5c46df
Michael Stramel <michael.stramel@maritz.com>
2018-02-22 11:18:37 -0600

    Docs: Update examples with expected output

--- example.js
+++ example.js
@@ -19,22 +19,62 @@ console.log(dotty.exists(object, ["a", "b", "z"])); // false
 console.log(dotty.get(object, "a.b.x")); // "y"
 console.log(dotty.get(object, ["a", "b", "x"])); // "y"
 console.log(dotty.get(object, "a.b.z")); // undefined
-console.log(dotty.get(object, ["a", "b", "z"])); // undefine
+console.log(dotty.get(object, "a.b.z.z"));  // undefined
+console.log(dotty.get(object, ["a", "b", "z"])); // undefined
 
 dotty.put(object, "a.b.hello", "hi");
 dotty.put(object, ["a", "c", "yo"], "sup");
 
-console.log(dotty.search(object, "a.b.*"));
-console.log(dotty.search(object, ["a", "b", "*"]));
-console.log(dotty.search(object, "a.*.x"));
-console.log(dotty.search(object, ["a", "*", "x"]));
-console.log(dotty.search(object, ["a", "*", /..+/]));
+console.log(dotty.search(object, "a.b.*")); // ["y", "hi"]
+console.log(dotty.search(object, ["a", "b", "*"])); // ["y", "hi"]
+console.log(dotty.search(object, "a.*.x")); // ["y", "z"]
+console.log(dotty.search(object, ["a", "*", "x"]));  // ["y", "z"]
+console.log(dotty.search(object, ["a", "*", /..+/]));  // ["hi", "sup"]
 
 console.log(dotty.remove(object, "a.b.x"));
 console.log(dotty.remove(object, "a.b.y"));
 
 console.log(dotty.deepKeys(object));
+/**
+  [
+    [ 'a' ],
+    [ 'a', 'b' ],
+    [ 'a', 'b', 'hello' ],
+    [ 'a', 'c' ],
+    [ 'a', 'c', 'x' ],
+    [ 'a', 'c', 'yo' ]
+  ]
+ */
+
 console.log(dotty.deepKeys(object, {leavesOnly: true}));
+/**
+  [
+    [ 'a', 'b', 'hello' ],
+    [ 'a', 'c', 'x' ],
+    [ 'a', 'c', 'yo' ]
+  ]
+ */
+
 console.log(dotty.deepKeys(object, {leavesOnly: true, asStrings: true}));
+/**
+  [
+    'a.b.hello',
+    'a.c.x',
+    'a.c.yo'
+  ]
+ */
 
 console.log(object);
+/**
+  {
+    a: {
+      b: {
+        hello: "hi"
+      },
+      c: {
+        x: "z',
+        yo: 'sup'
+      }
+    }
+  }
+ */

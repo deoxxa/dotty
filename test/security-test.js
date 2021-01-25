cd997d37917186c131be71501a698803f2b7ebdb
Michael Stramel <stramel@users.noreply.github.com>
2021-01-25 12:20:41 -0600

    Fix prototype pollution (#26)

---
+++ test/security-test.js
@@ -0,0 +1,29 @@
+var dotty = require("../lib/index"),
+  vows = require("vows"),
+  assert = require("assert");
+
+vows
+  .describe("security")
+  .addBatch({
+    "When we attempt to update the prototype": {
+      topic() {
+        var obj = {};
+        dotty.put(obj, "__proto__.polluted", "Muhahahaha");
+        return obj;
+      },
+      "it should not update": function (res) {
+        assert.equal(res.polluted, undefined);
+      },
+    },
+    "When we attempt to update the constructor prototype": {
+      topic() {
+        var obj = {};
+        dotty.put(obj, "constructor.prototype.polluted", "Muhahahaha");
+        return obj;
+      },
+      "it should not update": function (res) {
+        assert.equal(res.polluted, undefined);
+      },
+    },
+  })
+  .export(module);

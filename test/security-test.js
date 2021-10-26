88f61860dcc274a07a263c32cbe9d44c24ef02d7
Michael Stramel <stramel@users.noreply.github.com>
2021-10-26 10:46:00 -0500

    Address different method of prototype pollution (#32)
    * remove ds_store file
    
    * Fix pototype pollution
    
    * Bump deps
    
    * Update docs
    
    * Update workflow to test against latest node versions
    
    * Ensure put returns true
    
    * Remove node v6,8,10 from testing

--- test/security-test.js
+++ test/security-test.js
@@ -1,4 +1,4 @@
-var dotty = require("../lib/index"),
+const dotty = require("../lib/index"),
   vows = require("vows"),
   assert = require("assert");
 
@@ -7,17 +7,40 @@ vows
   .addBatch({
     "When we attempt to update the prototype": {
       topic() {
-        var obj = {};
+        const obj = {};
         dotty.put(obj, "__proto__.polluted", "Muhahahaha");
         return obj;
       },
       "it should not update": function (res) {
         assert.equal(res.polluted, undefined);
+        assert.equal(Object.prototype.polluted, undefined);
+      },
+    },
+    "When we attempt to update the prototype using an array": {
+      topic() {
+        const obj = {};
+        dotty.put(obj, ["__proto__", "polluted"], "Muhahahaha");
+        return obj;
+      },
+      "it should not update": function (res) {
+        assert.equal(res.polluted, undefined);
+        assert.equal(Object.prototype.polluted, undefined);
+      },
+    },
+    "When we attempt to update the prototype using a non-string": {
+      topic() {
+        const obj = {};
+        dotty.put(obj, [["__proto__"], "polluted"], "Muhahahaha");
+        return obj;
+      },
+      "it should not update": function (res) {
+        assert.equal(res.polluted, undefined);
+        assert.equal(Object.prototype.polluted, undefined);
       },
     },
     "When we attempt to update the constructor prototype": {
       topic() {
-        var obj = {};
+        const obj = {};
         dotty.put(obj, "constructor.prototype.polluted", "Muhahahaha");
         return obj;
       },
